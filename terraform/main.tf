e083d960a3632932b39f836b20a695c36ca741f3
Carlos Alan <carlosv@MacBook-Pro-de-Carlos.local>
2024-08-19 17:38:55 -0600

    Adding comments to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "us-east-1" 
 }
 
+#Creamos primero el bucket que almacenara el file de states y la tabla de dynamo para bloquear en caso de emergencia
+#Despues procederemos a quitar el comment de las siguientes lineas y crear el backend de nuestra infraestructura
+
 #terraform {
 #  backend "s3" {
 #    bucket         = "devops-directive-tf-state-quantum" 
